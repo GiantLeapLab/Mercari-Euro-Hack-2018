be87c6864132bfe2ef88be0c3c98dd0880aeb368
Lyhoshva Vlad <lyhoshvavlad@gmail.com>
2018-10-20 17:53:11 +0300

    Removed redndant migration

--- migrations/m181020_132045_update_requests_add_category_relations.php
+++ migrations/m181020_132045_update_requests_add_category_relations.php
@@ -10,12 +10,6 @@ class m181020_132045_update_requests_add_category_relations extends Migration
     // Use up()/down() to run migration code without a transaction.
     public function up()
     {
-        $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
-            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        }
-
         $this->addColumn('buy_request', 'category_id', $this->integer()->notNull());
         $this->addColumn('sell_request', 'category_id', $this->integer()->notNull());
 
