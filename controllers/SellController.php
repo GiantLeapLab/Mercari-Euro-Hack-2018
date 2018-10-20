3b15cad58f100db688149a8f84c613e43fc61707
igor.b <igor.b@giantleaplab.com>
2018-10-20 18:39:11 +0300

    scan app in progress

--- controllers/SellController.php
+++ controllers/SellController.php
@@ -8,6 +8,11 @@ use yii\web\Controller;
 
 class SellController extends Controller
 {
+    public function init() 
+    {
+        parent::init();
+        $this->enableCsrfValidation = false;
+    }
     public function actionIndex()
     {
         return $this->render('index');
@@ -16,5 +21,15 @@ class SellController extends Controller
     public function actionScan()
     {
         return $this->render('scan');
+    }
+    
+    public function actionCheckDemand()
+    {
+        $model = new \app\models\CheckDemandForm();
+        $model->load(Yii::$app->request->post());
+        
+        Yii::$app->response->format = 'json';
+        
+        return $model->check();
     }
 }
---
+++ models/CheckDemandForm.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * CheckDemandForm is the model behind the login form.
+ *
+ */
+class CheckDemandForm extends Model
+{
+    public $classes = [];
+    /**
+     * @return array the validation rules.
+     */
+    public function rules()
+    {
+        return [
+            [['classes'], 'required'],
+            [['classes'], 'each', 'rule' => ['string', 'length' => [2, 255]]],
+        ];
+    }
+    
+    public function check() {
+        //TODO
+        
+        //bulk data
+        return [
+            'cell-phone' => [
+                'minCost' => 45,
+                'maxCost' => 700,
+                'people' => 12,
+            ],
+            'laptop' => [
+                'minCost' => 300,
+                'maxCost' => 1300,
+                'people' => 5,
+            ],
+        ];
+    }
+}
